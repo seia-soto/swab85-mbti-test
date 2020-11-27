2a5b400ffe4cb19110772a8bb1ddc423ba19b7ba
Seia-Soto <seia@outlook.kr>
2020-11-27 20:49:33 +0900

    Add serving feature and add image compression post-processing plugin

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,6 +7,22 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-react-refresh',
     [
+      'snowpack-plugin-resize-images',
+      {
+        '**/images/**': {
+          webp: {
+            quality: 85
+          },
+          png: {
+            quality: 85
+          },
+          jpeg: {
+            quality: 85
+          }
+        }
+      }
+    ],
+    [
       '@snowpack/plugin-webpack',
       {}
     ]
